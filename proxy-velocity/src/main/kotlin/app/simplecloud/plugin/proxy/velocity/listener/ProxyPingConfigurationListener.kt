5ff0bf4d1af3dbc2474bd7085e4595b4e0d86214
Dominic <d151l@d151l.de>
2024-03-22 22:57:58 +0100

    update replacements

--- proxy-velocity/src/main/kotlin/app/simplecloud/plugin/proxy/velocity/listener/ProxyPingConfigurationListener.kt
+++ proxy-velocity/src/main/kotlin/app/simplecloud/plugin/proxy/velocity/listener/ProxyPingConfigurationListener.kt
@@ -2,6 +2,7 @@ package app.simplecloud.plugin.proxy.velocity.listener
 
 import app.simplecloud.plugin.proxy.velocity.ProxyVelocityPlugin
 import app.simplecloud.plugin.proxy.velocity.event.ProxyPingConfigurationEvent
+import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
 import net.kyori.adventure.text.TextReplacementConfig
 
@@ -9,17 +10,15 @@ class ProxyPingConfigurationListener(
     private val plugin: ProxyVelocityPlugin
 ) {
 
-    @Subscribe
+    @Subscribe(order = PostOrder.LAST)
     fun onProxyPingConfiguration(proxyPingConfigurationEvent: ProxyPingConfigurationEvent) {
         var motd = proxyPingConfigurationEvent.messageOfTheDay
 
         val showMaxPlayers = this.plugin.proxyServer.configuration.showMaxPlayers
-        val onlinePlayersCurrentProxy = this.plugin.proxyServer.allPlayers.size
+        val onlinePlayers = this.plugin.proxyServer.allPlayers.size
 
-        motd = motd.replaceText(TextReplacementConfig.builder().match("%ONLINE_PLAYERS%").replacement("soon").build())
-        motd = motd.replaceText(TextReplacementConfig.builder().match("%ONLINE_PLAYERS_CURRENT_PROXY%").replacement("$onlinePlayersCurrentProxy").build())
+        motd = motd.replaceText(TextReplacementConfig.builder().match("%ONLINE_PLAYERS%").replacement("$onlinePlayers").build())
         motd = motd.replaceText(TextReplacementConfig.builder().match("%MAX_PLAYERS%").replacement("$showMaxPlayers").build())
-        motd = motd.replaceText(TextReplacementConfig.builder().match("%PROXY%").replacement("soon").build())
 
         proxyPingConfigurationEvent.messageOfTheDay = motd
     }
