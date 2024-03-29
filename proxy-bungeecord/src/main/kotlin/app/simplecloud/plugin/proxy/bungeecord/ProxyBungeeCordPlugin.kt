9cb13724fab92485e9b1d679bf98ca5591162297
Dominic <d151l@d151l.de>
2024-03-29 14:00:37 +0100

    implement: tablist on bungee plugin

--- proxy-bungeecord/src/main/kotlin/app/simplecloud/plugin/proxy/bungeecord/ProxyBungeeCordPlugin.kt
+++ proxy-bungeecord/src/main/kotlin/app/simplecloud/plugin/proxy/bungeecord/ProxyBungeeCordPlugin.kt
@@ -1,17 +1,24 @@
 package app.simplecloud.plugin.proxy.bungeecord
 
+import app.simplecloud.plugin.proxy.bungeecord.handler.TabListHandler
+import app.simplecloud.plugin.proxy.bungeecord.listener.TabListListener
 import app.simplecloud.plugin.proxy.shared.config.YamlConfig
 import app.simplecloud.plugin.proxy.shared.config.motd.MotdConfiguration
 import app.simplecloud.plugin.proxy.shared.config.placeholder.PlaceHolderConfiguration
 import app.simplecloud.plugin.proxy.shared.config.tablis.TabListConfiguration
+import net.kyori.adventure.platform.bungeecord.BungeeAudiences
 import net.md_5.bungee.api.plugin.Plugin
-import kotlin.io.path.pathString
+
 
 class ProxyBungeeCordPlugin: Plugin() {
 
     lateinit var motdConfiguration: MotdConfiguration
     lateinit var tabListConfiguration: TabListConfiguration
     lateinit var placeHolderConfiguration: PlaceHolderConfiguration
+
+    val tabListHandler = TabListHandler(this)
+
+    private var adventure: BungeeAudiences? = null
 
     override fun onEnable() {
         val config = YamlConfig(this.dataFolder.path)
@@ -24,9 +31,25 @@ class ProxyBungeeCordPlugin: Plugin() {
 
         this.placeHolderConfiguration = config.load<PlaceHolderConfiguration>("placeholder")!!
         config.save("placeholder", this.placeHolderConfiguration)
+
+        this.adventure = BungeeAudiences.create(this);
+
+        this.tabListHandler.startTabListTask()
+
+        this.proxy.pluginManager.registerListener(this, TabListListener(this))
     }
 
     override fun onDisable() {
+        if(this.adventure != null) {
+            this.adventure!!.close();
+            this.adventure = null;
+        }
 
+        this.tabListHandler.stopTabListTask()
+    }
+
+    fun adventure(): BungeeAudiences {
+        checkNotNull(this.adventure) { "Cannot retrieve audience provider while plugin is not enabled" }
+        return adventure!!
     }
 }
---
+++ proxy-bungeecord/src/main/kotlin/app/simplecloud/plugin/proxy/bungeecord/handler/TabListHandler.kt
@@ -0,0 +1,105 @@
+package app.simplecloud.plugin.proxy.bungeecord.handler
+
+import app.simplecloud.plugin.proxy.bungeecord.ProxyBungeeCordPlugin
+import app.simplecloud.plugin.proxy.bungeecord.event.TabListConfigurationEvent
+import app.simplecloud.plugin.proxy.shared.config.tablis.TabList
+import app.simplecloud.plugin.proxy.shared.config.tablis.TabListGroup
+import app.simplecloud.plugin.proxy.shared.event.TabListPlayerConfiguration
+import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer
+import net.md_5.bungee.api.connection.ProxiedPlayer
+import net.md_5.bungee.api.scheduler.ScheduledTask
+import java.util.concurrent.TimeUnit
+
+
+class TabListHandler(
+    private val plugin: ProxyBungeeCordPlugin
+) {
+
+    private val miniMessage = MiniMessage.miniMessage()
+
+    private val tabListIndex = mutableMapOf<String, Int>()
+
+    private lateinit var task: ScheduledTask
+
+    fun startTabListTask() {
+        this.task = this.plugin.proxy.scheduler.schedule(this.plugin, {
+            this.plugin.proxy.players.forEach {
+                this.updateTabListForPlayer(it)
+            }
+            this.tabListIndex.forEach { (key, value) ->
+                this.tabListIndex[key] = value + 1
+            }
+        }, 1, this.plugin.tabListConfiguration.tabListUpdateTime, TimeUnit.MILLISECONDS)
+    }
+
+    fun stopTabListTask() {
+        if (!this::task.isInitialized)
+            return
+
+        this.task.cancel()
+    }
+
+    fun updateTabListForPlayer(player: ProxiedPlayer) {
+        val configuration = plugin.tabListConfiguration
+
+        val serviceName = player.server.info.name
+
+        var tabListGroup = configuration.groups.find { group ->
+            serviceName.startsWith(group.groupOrService, true)
+        }
+
+        if (tabListGroup == null) {
+            tabListGroup = configuration.groups.find { group ->
+                group.groupOrService.equals(
+                    serviceName,
+                    true
+                )
+            }
+        }
+
+        if (tabListGroup == null) {
+            tabListGroup = configuration.groups.find { group ->
+                group.groupOrService == "*"
+            }
+        }
+
+        if (tabListGroup == null) {
+            tabListGroup = TabListGroup(
+                "noValuedGroupOrServiceConfigurationsFround",
+                listOf(
+                    TabList(
+                    listOf("<red>No configuration found for this service"),
+                    listOf("<red>Please check your configuration file from the proxy plugin in the plugins folder"),
+                )
+                )
+            )
+        }
+
+        if (tabListGroup.tabLists.size <= tabListIndex.getOrDefault(tabListGroup.groupOrService, 0) + 1)
+            tabListIndex[tabListGroup.groupOrService] = 0
+
+        if (!tabListIndex.containsKey(tabListGroup.groupOrService))
+            tabListIndex[tabListGroup.groupOrService] = 0
+
+        val tabList = tabListGroup.tabLists[tabListIndex.getOrDefault(tabListGroup.groupOrService, 0)]
+
+        val header = this.miniMessage.deserialize(tabList.header.joinToString("<newline>"))
+        val footer = this.miniMessage.deserialize(tabList.footer.joinToString("<newline>"))
+
+        val tabListPlayerConfiguration = this.plugin.proxy.pluginManager.callEvent(
+            TabListConfigurationEvent(
+                TabListPlayerConfiguration(
+                    header,
+                    footer
+                ), player
+            )
+        ).tabListConfiguration
+
+        val audience = this.plugin.adventure().player(player)
+        audience.sendPlayerListHeaderAndFooter(
+            tabListPlayerConfiguration.header,
+            tabListPlayerConfiguration.footer
+        )
+    }
+}
