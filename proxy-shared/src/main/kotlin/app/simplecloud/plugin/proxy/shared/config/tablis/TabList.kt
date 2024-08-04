94f9efe99eafd833e1649c06c67d2ae397b36aaa
Dominic Kaemereit <d151l@d151l.de>
2024-08-04 22:51:53 +0200

    fix: outdated placeholder on default tablist configuration

--- proxy-shared/src/main/kotlin/app/simplecloud/plugin/proxy/shared/config/tablis/TabList.kt
+++ proxy-shared/src/main/kotlin/app/simplecloud/plugin/proxy/shared/config/tablis/TabList.kt
@@ -7,7 +7,7 @@ data class TabList(
     val header: List<String> = listOf(
         "<dark_gray>                                                                        <dark_gray>",
         "<aqua>SimpleCloud <dark_gray>» <gray>Simplify <white><italic>your</italic> <gray>network",
-        "<dark_aqua>Online <dark_gray>» <gray><online_players><dark_gray>/<gray><max_players> <dark_gray>┃ <dark_aqua>Server <dark_gray>» <gray>%SERVICE_NAME%",
+        "<dark_aqua>Online <dark_gray>» <gray><online_players><dark_gray>/<gray><max_players> <dark_gray>┃ <dark_aqua>Server <dark_gray>» <gray><server_name>",
         "<dark_gray>"
     ),
     val footer: List<String> = listOf(
