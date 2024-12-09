3042773425890b2b84a5b73fe8bc256f9cfbf03f
Dominic Kaemereit <d151l@d151l.de>
2024-12-09 22:12:30 +0100

    refactor: port join request from velocity to bungeecord

--- proxy-bungeecord/src/main/kotlin/app/simplecloud/plugin/proxy/bungeecord/listener/ServerPreConnectListener.kt
+++ proxy-bungeecord/src/main/kotlin/app/simplecloud/plugin/proxy/bungeecord/listener/ServerPreConnectListener.kt
@@ -3,6 +3,7 @@ package app.simplecloud.plugin.proxy.bungeecord.listener
 import app.simplecloud.plugin.proxy.bungeecord.ProxyBungeeCordPlugin
 import app.simplecloud.plugin.proxy.bungeecord.toBaseComponent
 import kotlinx.coroutines.runBlocking
+import net.md_5.bungee.api.config.ServerInfo
 import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.event.ServerConnectEvent
 import net.md_5.bungee.api.plugin.Listener
@@ -18,7 +19,45 @@ class ServerPreConnectListener(
     @EventHandler(priority = EventPriority.HIGH)
     fun handle(event: ServerConnectEvent) {
         val player = event.player
+        val serverInfo = event.target
 
+        checkAllowProxyJoin(player, event)
+        checkAllowServerSwitch(player, event, serverInfo)
+
+    }
+    private fun checkAllowServerSwitch(player: ProxiedPlayer, event: ServerConnectEvent, server: ServerInfo) {
+        val serviceName = server.name
+        val split = serviceName.split("-")
+
+        val numericalId = split.last()
+
+        val mutableList = split.toMutableList()
+        mutableList.removeLast()
+        val groupName = mutableList.joinToString("-")
+
+        runBlocking {
+            val joinStateName =
+                proxyPlugin.proxyPlugin.joinStateHandler.getJoinStateAtService(groupName, numericalId.toLong())
+            val joinState = proxyPlugin.proxyPlugin.joinStateConfiguration.joinStates.find { it.name == joinStateName }
+
+            if (joinState == null) {
+                logger.warning("No join state found for server.")
+                denyAccess(player, proxyPlugin.proxyPlugin.messagesConfiguration.kickMessage.noJoinState, event)
+                return@runBlocking
+            }
+
+            if (joinState.joinPermission != "" && !player.hasPermission(joinState.joinPermission)) {
+                denyAccess(
+                    player,
+                    proxyPlugin.proxyPlugin.messagesConfiguration.kickMessage.noPermission,
+                    event
+                )
+                return@runBlocking
+            }
+        }
+    }
+
+    private fun checkAllowProxyJoin(player: ProxiedPlayer, event: ServerConnectEvent) {
         val localState = this.proxyPlugin.proxyPlugin.joinStateHandler.localState
         val joinState = this.proxyPlugin.proxyPlugin.joinStateConfiguration.joinStates.find { it.name == localState }
 
@@ -28,10 +67,10 @@ class ServerPreConnectListener(
             return
         }
 
-        if (player.hasPermission(joinState.joinPermission)) {
+        if (!player.hasPermission(joinState.joinPermission) && joinState.joinPermission != "") {
             denyAccess(
                 player,
-                this.proxyPlugin.proxyPlugin.messagesConfiguration.kickMessage.networkMaintenance,
+                this.proxyPlugin.proxyPlugin.messagesConfiguration.kickMessage.noPermission,
                 event
             )
             return
