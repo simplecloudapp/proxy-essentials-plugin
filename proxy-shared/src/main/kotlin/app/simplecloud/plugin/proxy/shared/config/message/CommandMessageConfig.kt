fe43bccf0dd0eec4fe6f26ddcb73ded12a8b24f5
Dominic Kaemereit <d151l@d151l.de>
2024-12-09 22:14:56 +0100

    refactor: update default configurations

--- proxy-shared/src/main/kotlin/app/simplecloud/plugin/proxy/shared/config/message/CommandMessageConfig.kt
+++ proxy-shared/src/main/kotlin/app/simplecloud/plugin/proxy/shared/config/message/CommandMessageConfig.kt
@@ -4,10 +4,10 @@ import org.spongepowered.configurate.objectmapping.ConfigSerializable
 
 @ConfigSerializable
 data class CommandMessageConfig(
-    val joinStateServiceUpdateSuccess: String = "Join state of service updated successfully.",
-    val joinStateServiceUpdateFailure: String = "Failed to update join state of service.",
-    val joinStateServiceUpdateNoChange: String = "Join state of service did not change.",
-    val joinStateGroupUpdateSuccess: String = "Join state of group updated successfully.",
-    val joinStateGroupUpdateFailure: String = "Failed to update join state of group.",
-    val joinStateGroupUpdateNoChange: String = "Join state of group did not change."
+    val joinStateServiceUpdateSuccess: String = "<color:#38bdf8><bold>⚡</bold></color> <color:#ffffff>Join state of service updated successfully.",
+    val joinStateServiceUpdateFailure: String = "<color:#38bdf8><bold>⚡</bold></color> <color:#ffffff>Failed to update join state of service.",
+    val joinStateServiceUpdateNoChange: String = "<color:#38bdf8><bold>⚡</bold></color> <color:#ffffff>Join state of service did not change.",
+    val joinStateGroupUpdateSuccess: String = "<color:#38bdf8><bold>⚡</bold></color> <color:#ffffff>Join state of group updated successfully.",
+    val joinStateGroupUpdateFailure: String = "<color:#38bdf8><bold>⚡</bold></color> <color:#ffffff>Failed to update join state of group.",
+    val joinStateGroupUpdateNoChange: String = "<color:#38bdf8><bold>⚡</bold></color> <color:#ffffff>Join state of group did not change."
 )
