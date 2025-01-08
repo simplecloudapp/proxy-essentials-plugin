74f972a32b257126b364efed298761bf46a48566
Philipp <p.eistrach@gmail.com>
2025-01-08 19:55:16 +0100

    feat: modrinth publishing

--- proxy-bungeecord/build.gradle.kts
+++ proxy-bungeecord/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    alias(libs.plugins.minotaur)
+}
+
 dependencies {
     api(project(":proxy-shared"))
 
@@ -11,4 +15,29 @@ dependencies {
 
     implementation(rootProject.libs.command.cloud.core)
     implementation(rootProject.libs.command.cloud.bungeecord)
+}
+
+modrinth {
+    token.set(project.findProperty("modrinthToken") as String? ?: System.getenv("MODRINTH_TOKEN"))
+    projectId.set("WzfN2mLK")
+    versionNumber.set(rootProject.version.toString())
+    versionType.set("beta")
+    uploadFile.set(tasks.shadowJar)
+    gameVersions.addAll(
+        "1.20",
+        "1.20.1",
+        "1.20.2",
+        "1.20.3",
+        "1.20.4",
+        "1.20.5",
+        "1.20.6",
+        "1.21",
+        "1.21.1",
+        "1.21.2",
+        "1.21.3",
+        "1.21.4"
+    )
+    loaders.add("bungeecord")
+    changelog.set("https://docs.simplecloud.app/changelog")
+    syncBodyFrom.set(rootProject.file("README.md").readText())
 }
