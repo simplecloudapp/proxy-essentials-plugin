ae5b071994e2b82b74be239e52b518224496fd0b
fllipeis <p.eistrach@gmail.com>
2025-07-12 02:27:08 +0200

    feat: waterfall

--- proxy-bungeecord/build.gradle.kts
+++ proxy-bungeecord/build.gradle.kts
@@ -48,6 +48,7 @@ modrinth {
         "1.21.7",
     )
     loaders.add("bungeecord")
+    loaders.add("waterfall")
     changelog.set("https://docs.simplecloud.app/changelog")
     syncBodyFrom.set(rootProject.file("README.md").readText())
 }
