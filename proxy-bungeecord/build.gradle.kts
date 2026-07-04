14c2abe398bf2d50e2c298843b3595096cbdfaca
fllipeis <p.eistrach@gmail.com>
2026-07-04 20:11:38 +0200

    chore: prepare Modrinth release publishing

--- proxy-bungeecord/build.gradle.kts
+++ proxy-bungeecord/build.gradle.kts
@@ -24,7 +24,7 @@ modrinth {
     token.set(project.findProperty("modrinthToken") as String? ?: System.getenv("MODRINTH_TOKEN"))
     projectId.set("WzfN2mLK")
     versionNumber.set(rootProject.version.toString())
-    versionType.set("beta")
+    versionType.set("release")
     uploadFile.set(tasks.shadowJar)
     gameVersions.addAll(
         
